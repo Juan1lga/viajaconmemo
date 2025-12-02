b57edc3f0ec693d312edc8717eb237d2cf308293
Juan <juan.ariel@example.com>
2025-12-02 08:50:41 -0500

    chore(backend): actualizar CORS para viajaconmemoya.com

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
 
 const isProd = process.env.NODE_ENV === 'production';
 
